206c179a78275c03fa9013728b88888bc7081056
Yoshinori Yasuda <a2041yy@aiit.ac.jp>
2020-05-23 22:30:21 +0000

    Finish ch07

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,33 @@
 class UsersController < ApplicationController
+  # GET /users/:id
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  # GET /users/new
   def new
+    @user = User.new
+  end
+  
+  #POST /users
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      # GET "/users/#{@user.id}"
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+      # redirect_to user_path(@user)
+      # redirect_to user_path(@user.id)
+      # redirect_to user_path(1)
+      #             => /users/1
+    else
+      render 'new'
+    end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
