42f286ba4d42cc683abcb219efc3a1fdbf3a050f
Yoshinori Yasuda <a2041yy@aiit.ac.jp>
2020-06-11 16:45:06 +0000

    change delivery_method to :test again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,17 +66,15 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :test
   host = 'aiit-sample-app-a2041yy.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    # :user_name      => ENV['SENDGRID_USERNAME'],
-    # :password       => ENV['SENDGRID_PASSWORD'],
-    :user_name      => 'app170751653@heroku.com',
-    :password       => 'wlxqm7d11007',
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
