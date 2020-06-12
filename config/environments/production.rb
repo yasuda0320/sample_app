3aaaa676f97c82aa7f2bd0210556834069080b26
Yoshinori Yasuda <a2041yy@aiit.ac.jp>
2020-06-12 04:49:58 +0000

    heroku run rails db:seed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :test
   host = 'aiit-sample-app-a2041yy.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
