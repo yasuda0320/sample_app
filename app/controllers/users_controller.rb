52a71c52ab3aea5a0197933af85564bb557c6780
Yoshinori Yasuda <a2041yy@aiit.ac.jp>
2020-06-13 12:35:03 +0000

    Finish ch13 (skip 13.4.4)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   # GET /users/:id
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
   
   # GET /users/new
@@ -54,29 +55,21 @@ class UsersController < ApplicationController
   end
   
   private
-  
+
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
     end
-    
-    # beforeアクション
-  
-    # ログイン済みユーザーかどうか確認
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
-  
-    # 正しいユーザーかどうか確認
+
+    # beforeフィルター
+
+    # 正しいユーザーかどうかを確認
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
     end
-    
-    # 管理者かどうか確認
+
+    # 管理者かどうかを確認
     def admin_user
       redirect_to(root_url) unless current_user.admin?
     end
